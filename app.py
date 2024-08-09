0e8a48942088eb93484fd87aedac6b67c385d865
mng990 <ryoo0504@gmail.com>
2024-08-10 01:36:54 +0900

    fix: flask host:0.0.0.0 옵션 추가

--- app.py
+++ app.py
@@ -17,4 +17,4 @@ def generate_text_route():
         return jsonify({'error': str(e)}), 500
 
 if __name__ == '__main__':
-    app.run(port=5001, debug=True)
+    app.run(host='0.0.0.0', port=5001, debug=True)
