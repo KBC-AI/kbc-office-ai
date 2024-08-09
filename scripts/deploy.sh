73825524d761ccb2adc57f68192377125d79a4e9
mng990 <ryoo0504@gmail.com>
2024-08-09 20:23:59 +0900

    test: CI/CD 환경 구성 및 배포 테스트

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -9,24 +9,8 @@ runNetwork(){
   fi
 }
 
-serviceDown(){
-  if [ $(docker ps -a -q -f name=$CONTAINER_NAME) ]; then
-      echo "컨테이너 $CONTAINER_NAME 종료 및 삭제 중..."
-
-      IMAGE_ID=$(docker images -q $CONTAINER_NAME)
-
-      if [ "$IMAGE_ID" ]; then
-          echo "이미지 $CONTAINER_NAME 삭제 중..."
-
-      docker rmi -f $IMAGE_ID
-      fi
-  fi
-}
-
 cd /home/ubuntu/deploy/ai
 
 runNetwork
-
-serviceDown gpt4o
 
 docker-compose up -d --build
